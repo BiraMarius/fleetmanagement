c1243e0041123c88d7e6df3c22e95f0b99ac87bb
MariusB <bira_marius_gabriel@yahoo.com>
2024-01-24 20:15:05 +0200

    15

--- src/main/java/com/biramarius/fleetmanagement/ApplicationController.java
+++ src/main/java/com/biramarius/fleetmanagement/ApplicationController.java
@@ -11,8 +11,29 @@ public class ApplicationController {
         return "index";
     }
 
-    @GetMapping("/widgets")
-    public String widget(){
-        return "widgets";
+    @GetMapping("/hr")
+    public String hr(){
+        return "/hr/index";
     }
+
+    @GetMapping("/accounts")
+    public String accounts(){ return "/accounts/index";}
+
+    @GetMapping("/it")
+    public String itdep(){ return "/it/index";}
+
+    @GetMapping("/payroll")
+    public String payroll(){ return "/payroll/index";}
+
+    @GetMapping("/vehicles")
+    public String vehicles(){ return "/vehicles/index";}
+
+    @GetMapping("/fleet")
+    public String fleet(){ return "/fleet/index";}
+
+    @GetMapping("/helpdesk")
+    public String helpdesk(){ return "/helpdesk/index";}
+
+    @GetMapping("/parameters")
+    public String parameters(){ return "/parameters/index";}
 }
